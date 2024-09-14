1ddfde1320a9bc7811ec53b5d183385de9541915
Srijan Mani Tripathi <srijanmanitripathi2002@gmail.com>
2024-09-15 02:14:03 +0530

    sign JWT token

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,6 +5,7 @@ const _config = {
     port: process.env.PORT,
     dataBaseUrl: process.env.MONGOOSE_CONNECTION_STRING,
     env: process.env.NODE_ENV,
+    jwtSecret: process.env.JWT_SECRET,
 };
 
 export const config = Object.freeze(_config);
--- src/user/userController.ts
+++ src/user/userController.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
 import bcrypt from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
 
 // Create an user
 const createUser = async (
@@ -28,7 +30,19 @@ const createUser = async (
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    res.json({message: "User registered"});
+    // Create new user
+
+    const newUser = await userModel.create({
+        name,
+        email,
+        password: hashedPassword,
+    });
+
+    // Create token using JWT
+
+    const token = sign({sub: newUser._id}, config.jwtSecret as string, {expiresIn: "7d"});
+
+    res.json({accessToken: token});
 };
 
 
